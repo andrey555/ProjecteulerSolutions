8866b95172483e83ed9f8df5848dee433589dac1
andrey_v.v <andrey_v.v@ukr.net>
2015-09-27 17:58:38 +0300

    andrey_v.v@ukr.net

--- src/problems1to10/problem3.java
+++ src/problems1to10/problem3.java
@@ -1,1 +1,43 @@
-//
+package problems1to10;
+
+/**
+ * Solution to Project Euler problem 1
+ * Created by andr on 27.09.15.
+ */
+public class problem3 {
+
+    public static void main(String[] args) {
+
+
+        long n = 600851475143L;
+        while (true) {
+            long p = smallestFactor(n);
+            if (p < n)
+                n /= p;
+            else
+                break;
+                System.out.println(n);
+
+        }
+
+    }
+
+        private static long smallestFactor(long n) {
+            if (n <= 1)
+                throw new IllegalArgumentException();
+            for (long i = 2, end = (long)Math.sqrt(n); i <= end; i++) {
+                if (n % i == 0)
+                    return i;
+            }
+            return n;  // n itself is prime
+        }
+
+
+
+    }
+
+
+
+
+
+
