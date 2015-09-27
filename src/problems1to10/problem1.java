5e344d16911825ad7e15e901c2fcda61b09aac3c
andrey_v.v <andrey_v.v@ukr.net>
2015-09-27 16:40:27 +0300

    andrey_v.v@ukr.net

---
+++ src/problems1to10/problem1.java
@@ -0,0 +1,20 @@
+package problems1to10;
+
+/**
+ * Solution to Project Euler problem 1
+ * Created by andr on 27.09.15.
+ */
+
+public class problem1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        }
+        System.out.println(sum);
+    }
+}
+
+
+
